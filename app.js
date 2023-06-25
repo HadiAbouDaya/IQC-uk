fecdca3939dfc80fafb074d346fdad9b3c669bfd
HadiAbouDayaMA <hadi.aboudaya@mobileartsme.com>
2023-06-25 22:13:36 +0300

    second Commit

--- app.js
+++ app.js
@@ -1,11 +1,21 @@
 document.getElementById('get-price').addEventListener('click', function() {
-    var input1 = parseInt(document.getElementById('input1').value);
-    var dropdown1 = parseInt(document.getElementById('dropdown1').value);
-    var dropdown2 = parseInt(document.getElementById('dropdown2').value);
-    var dropdown3 = parseInt(document.getElementById('dropdown3').value);
-  
-    var result = input1 + dropdown1 + dropdown2 + dropdown3;
-  
-    document.getElementById('result').innerText = result;
-  });
-  
+  var numEmployees = parseInt(document.getElementById('numEmployees').value);
+  var riskLevel = document.getElementById('riskLevel').value;
+
+  var FeesPerAMD = numEmployees <= 45 ? 750 : 1500;
+  var LicenseFee = 2000;
+
+  var auditDaysMap = {
+    '1': [1.50, 2.00, 2.50, 3.00, 4.00, 5.00, 6.00, 7.00, 8.00, 9.00, 10.00, 11.00, 12.00, 13.00, 14.00, 15.00, 16.00, 17.00, 18.00, 19.00, 20.00, 21.00, 22.00],
+    '2': [2.50, 3.00, 3.00, 3.50, 4.00, 4.50, 5.00, 5.50, 6.00, 7.00, 8.00, 9.00, 10.00, 11.00, 12.00, 12.00, 13.00, 14.00, 15.00, 16.00, 17.00, 19.00, 20.00],
+    '3': [2.50, 3.00, 3.50, 4.50, 5.50, 6.00, 7.00, 8.00, 9.00, 10.00, 11.00, 12.00, 13.00, 15.00, 16.00, 17.00, 18.00, 19.00, 20.00, 21.00, 23.00, 25.00, 27.00],
+    '4': [3.00, 3.50, 4.50, 5.50, 7.00, 8.00, 9.00, 11.00, 12.00, 13.00, 15.00, 16.00, 17.00, 19.00, 20.00, 21.00, 23.00, 25.00, 27.00, 28.00, 30.00, 32.00, 34.00]
+  };
+
+  var auditDays = auditDaysMap[riskLevel][numEmployees-1]; // Subtracting 1 because array indices start at 0
+  var totalPrice = LicenseFee + FeesPerAMD * auditDays;
+
+  document.getElementById('auditDays').innerText = auditDays;
+  document.getElementById('totalPrice').innerText = totalPrice;
+});
+
